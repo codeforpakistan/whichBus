8d12d548f187d57aff0cab138c9d37aae59a748f
Saad Masood <itsmesmd@gmail.com>
2013-10-05 13:52:52 +0500

    typo fixed

--- app/models/busstop.rb
+++ app/models/busstop.rb
@@ -1,4 +1,4 @@
 class Busstop < ActiveRecord::Base
-  has_many :routes_busstops
+  has_many :route_busstops
   has_many :routes, through: :route_busstops
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,5 @@
 class Route < ActiveRecord::Base
-  has_many :routes_busstops
+  has_many :route_busstops
   has_one :admin
   has_many :routes, through: :route_busstops
 end
