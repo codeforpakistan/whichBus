c21c811d786e00c0716b121d7523730ee462c5ac
Saad Masood <itsmesmd@gmail.com>
2013-11-10 03:42:48 +0500

    working on api not complete

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,31 +1,29 @@
 class Api::ApiController < ApplicationController
-  respond_to :json
-  def showRoute
-    begin
-        @routes = Route.find(params[:id])
-        @busstops = @routes.busstops
-        render :json => {:routes => @busstops}
-    rescue ActiveRecord::RecordNotFound => e
-        render :json =>
-        {
-            status: 'Failed',
-            results: 'No Record Found'
-            
-        }
+    respond_to :json
+    def showRoute
+        begin
+            @routes = Route.find(params[:id])
+            @busstops = @routes.busstops
+            render :json => {:routes => @busstops}
+        rescue ActiveRecord::RecordNotFound => e
+            render :json =>
+            {
+                status: 'Failed',
+                results: 'No Record Found'
+
+            }
+        end
     end
-    
-    
+
+
     def savePolyLineData
-#       begin
-          busstopID = params[:busStopID]
-          routeID = params[:routeID]
-          nextBusstop = params[:nextBusStopID]
-          render :json => { :status => 'OK'}
+        busstopID = params[:busStopID]
+        routeID = params[:routeID]
+        nextBusstop = params[:nextBusStopID]
+        route = Route.find(routeID)
+        busStopOnRoute = RouteBusstop.where("route_id = #{routeID} AND busstop_id = #{busstopID}")
+        render :json => { :status => 'OK', :route => route, :RouteBusstop => busStopOnRoute}
 
-       
-        
+
     end
-
-    
-  end
 end
