c070125412181a7d04402006a24152c80c7074bf
Saad Masood <itsmesmd@gmail.com>
2013-12-22 14:46:33 +0500

    work related to graph creation pending.

--- app/models/busstop_node.rb
+++ app/models/busstop_node.rb
@@ -1,8 +1,8 @@
-class BusstopNode < Busstop
-	attr_accessor :id, :neighbours
+class BusstopNode
+	attr_accessor :id, :neighbours, :busstop
 	@@graph = Array.new
 	def initialize(id = 0, *neighbour)
-		super
+		@busstop = Busstop.new
 		@id = -1
 		if not @id == 0
 			@id = id			
@@ -26,5 +26,23 @@ class BusstopNode < Busstop
 			end
 			print "\n"
 		end
+	end
+
+	def getBusstop(id)
+		@id = id
+		return @busstop = Busstop.find(id)
+	end
+
+	def self.createGraph
+		allBusstops = Busstop.all
+		allBusstops.each do |currentBusstop|
+			@@graph << newNode = BusstopNode.new
+			newNode.busstop = currentBusstop
+			newNode.id = currentBusstop.id
+			#Assigning Neighbours
+			
+
+		end
+		
 	end
 end
