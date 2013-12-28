e342d48cba81cae522d985526ededee3ecb2c3da
Tayyab Zahid <tayyabzahid96@gmail.com>
2013-12-28 10:46:37 +0500

    cookie conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124184859) do
+ActiveRecord::Schema.define(version: 20131228054250) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -93,6 +93,16 @@ ActiveRecord::Schema.define(version: 20131124184859) do
     t.string   "routeSourceName"
     t.string   "routeDestName"
   end
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "welcomes", force: true do |t|
     t.datetime "created_at"
